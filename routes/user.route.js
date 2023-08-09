64cd1085381a9d373d767af4cc29d0aea88a2f12
Santosh Kumar Singh <101744190+Santoshs0293@users.noreply.github.com>
2023-08-09 11:36:46 +0530

    Your commit message here

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,55 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated, ensureNotAuthenticated } = require('../utils/authMiddleware');
+const { ensureAuthenticated } = require('../utils/authMiddleware');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const User = require('../models/user.model');
 
-// Protected route for authenticated users
-router.get('/profile', ensureAuthenticated, (req, res, next) => {
-  // Render the profile page
+// Profile Page
+router.get('/profile', ensureAuthenticated, async (req, res, next) => {
+  const person = req.user;
+  res.render('profile', { person });
 });
 
-// Route accessible only for not authenticated users
-router.get('/login', ensureNotAuthenticated, (req, res, next) => {
-  // Render the login page
+// Edit User Profile
+router.post('/edit-profile', ensureAuthenticated, [
+  // Add any validation rules here if needed
+], async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Handle validation errors
+      // You can render the profile page with errors here
+      return;
+    }
+
+    const { name, password, confirmPassword } = req.body;
+
+    // Find the user by ID
+    const user = await User.findById(req.user._id);
+
+    // Update user's name
+    user.name = name;
+
+    // Update user's password if provided
+    if (password) {
+      if (password !== confirmPassword) {
+        req.flash('error', 'Passwords do not match');
+        res.redirect('/user/profile');
+        return;
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    // Save the updated user object
+    await user.save();
+
+    req.flash('success', 'Profile updated successfully');
+    res.redirect('/user/profile');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
